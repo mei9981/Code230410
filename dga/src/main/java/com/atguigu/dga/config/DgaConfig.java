14f280bc3d0b8f1f5bc7d3b988ca6b3546204c91
marry <atguigutest@163.com>
2023-08-21 15:18:04 +0800

    springboot 10

---
+++ dga/src/main/java/com/atguigu/dga/config/DgaConfig.java
@@ -0,0 +1,30 @@
+package com.atguigu.dga.config;
+
+import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
+import org.apache.hadoop.hive.metastore.api.MetaException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Smexy on 2023/8/21
+ */
+@Configuration
+public class DgaConfig
+{
+    @Value("${hive.metastore.uris}")
+    private String uri;
+    @Bean
+    public HiveMetaStoreClient createHiveMetaStoreClient()  {
+
+        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+        conf.set("hive.metastore.uris",uri);
+        HiveMetaStoreClient client = null;
+        try {
+            client = new HiveMetaStoreClient(conf);
+        } catch (MetaException e) {
+            throw new RuntimeException(e);
+        }
+        return client;
+    }
+}
--- dga/src/test/java/com/atguigu/dga/DgaApplicationTests.java
+++ dga/src/test/java/com/atguigu/dga/DgaApplicationTests.java
@@ -1,14 +1,35 @@
 package com.atguigu.dga;
 
+import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.thrift.TException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class DgaApplicationTests
 {
 
+    @Autowired
+    private HiveMetaStoreClient client;
     @Test
-    void contextLoads() {
+    void testHiveClient() throws TException {
+
+        //获取所有的库
+        List<String> dbs = client.getAllDatabases();
+        System.out.println(dbs);
+
+        //获取某个库下所有的表
+        List<String> tables = client.getAllTables("gmall");
+        System.out.println(tables);
+
+        //获取某个表的元数据信息
+        Table table = client.getTable("gmall", "ods_log_inc");
+        System.out.println(table);
+
     }
 
 }
